f79c59618d528605e489cb355091035506e9c9b2
David Betteridge <david.betteridge@proactis.com>
2019-01-09 20:18:34 +0000

    951

--- David/David/Program.cs
+++ David/David/Program.cs
@@ -1,14 +1,13 @@
-﻿public class Program { public static void Main() {
+﻿public class P{public static void Main(){
 var a="I'll tell you what I want, what I really, really want\r\nSo tell me what you want, what you really, really want\r\n";
-var c="\r\nIf you wanna be my lover, you ";
+var z="\r\nIf you ";
+var c=$"{z}wanna be my lover, you ";
 var b=", (ha) I wanna";
 var e= $"I wanna{b}{b}{b}, (ha)\r\nI wanna really, really, really wanna zigazig ah";
 var f="otta get with my friends";
 System.Console.Write($@"Ha ha ha ha ha
 Yo, {a}{a}{e}
-
-If you want my future, forget my past
-If you wanna get with me, better make it fast
+{z}want my future, forget my past{z}wanna get with me, better make it fast
 Now don't go wasting my precious time
 Get your act together we could be just fine
 
@@ -21,7 +20,6 @@ Oh, what do you think about that
 Now you know how I feel
 Say, you can handle my love, are you for real
 (Are you for real)
-I won't be hasty, I'll give you a try
-If you really bug me then I'll say goodbye
+I won't be hasty, I'll give you a try{z}really bug me then I'll say goodbye
 
 Yo {a}{e}");}}
