52ddaba66df591086e3851ee0ceb9d726beb9d09
David Betteridge <david.betteridge@proactis.com>
2019-01-09 20:25:19 +0000

    937

--- David/David/Program.cs
+++ David/David/Program.cs
@@ -1,11 +1,4 @@
-﻿public class P{public static void Main(){
-var a="I'll tell you what I want, what I really, really want\r\nSo tell me what you want, what you really, really want\r\n";
-var z="\r\nIf you ";
-var c=$"{z}wanna be my lover, you ";
-var b=", (ha) I wanna";
-var e= $"I wanna{b}{b}{b}, (ha)\r\nI wanna really, really, really wanna zigazig ah";
-var f="otta get with my friends";
-System.Console.Write($@"Ha ha ha ha ha
+﻿public class P{public static void Main(){var a="I'll tell you what I want, what I really, really want\r\nSo tell me what you want, what you really, really want\r\n";var z="\r\nIf you ";var c=$"{z}wanna be my lover, you ";var b=", (ha) I wanna";var e= $"I wanna{b}{b}{b}, (ha)\r\nI wanna really, really, really wanna zigazig ah";var f="otta get with my friends";System.Console.Write($@"Ha ha ha ha ha
 Yo, {a}{a}{e}
 {z}want my future, forget my past{z}wanna get with me, better make it fast
 Now don't go wasting my precious time
